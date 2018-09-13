1087559857c72889af7d3cc03134747dbe03f732
dasun <dasun@sprii.com>
2018-09-13 13:01:41 +0530

    adding ci scripts

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 # integration-tests.sh
 echo "Starting integration tests..."
+res1=$(curl -s -o /dev/null -w "%{http_code}" http://$1/)
+
+if [ "$res1" != "200" ]; then
+ echo "Path / test failed. Aborting..."
+ exit 1
+fi
+
+res2=$(curl -X POST -H "Content-Type: application/json" -d '{"text":"ass"}' -s -o /dev/null -w "%{http_code}" "http://$1/api/todos")
+if [ "$res2" != "200" ]; then
+ echo "POST test failed. Aborting..."
+ exit 1
+fi
 
 echo "Integration tests succeeded."
--- tests/production_test.sh
+++ tests/production_test.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # production-tests.sh
 echo "Starting production tests..."
+res1=$(curl -s -o /dev/null -w "%{http_code}" http://$1/)
 
+if [ "$res1" != "200" ]; then
+ echo "Path / test failed. Aborting..."
+ exit 1
+fi
+
+res2=$(curl -X POST -H "Content-Type: application/json" -d '{"text":"ass"}' -s -o /dev/null -w "%{http_code}" "http://$1/api/todos")
+if [ "$res2" != "200" ]; then
+ echo "POST test failed. Aborting..."
+ exit 1
+fi
 echo "Production tests succeeded."
